7f89feb1f4a4c201853dc938af2e15c546248152
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2020-10-08 11:11:22 -0300

    feat: adds btn, card, header, input and numberContainer components

---
+++ components/Btn.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { View, Button, StyleSheet } from "react-native";
+
+const Btn = (props) => {
+  return (
+    <View style={{ ...styles.button, ...props.style }}>
+      <Button {...props} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: 110,
+    maxWidth: '50%',
+    paddingHorizontal: 5,
+  },
+});
+
+export default Btn;
